991385bb62e33227c01b2ef5004e18ec9fb84362
Gunn Renate Dolsvaag <gunnrd@uia.no>
2021-04-29 10:45:45 +0200

    Changed adapter name

--- app/src/main/java/com/example/tictactoe/GameAdapter.kt
+++ app/src/main/java/com/example/tictactoe/GameAdapter.kt
@@ -5,13 +5,13 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tictactoe.databinding.GameLayoutBinding
 
-class GameManagerAdapter() : RecyclerView.Adapter<GameManagerAdapter.ViewHolder>() {
+class GameAdapter() : RecyclerView.Adapter<GameAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(GameLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
-    override fun onBindViewHolder(holder: GameManagerAdapter.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: GameAdapter.ViewHolder, position: Int) {
         TODO("Not yet implemented")
     }
 
